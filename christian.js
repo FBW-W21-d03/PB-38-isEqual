fbd9294baebba78888b4872026175dbee9d2f54c
Christian Heinrich <christian.heinrich@digitalcareerinstitute.org>
2022-01-04 09:31:28 +0100

    Lösung von Christian

--- christian.js
+++ christian.js
@@ -1,3 +1,4 @@
+// Teil 1
 function isEqual(number) {
   if (number % 2 == 0) {
     console.log("Die Zahl " + number + " ist gerade.");
@@ -6,5 +7,6 @@ function isEqual(number) {
   }
 }
 
+// Teil 2
 isEqual(14);
 isEqual(17);
