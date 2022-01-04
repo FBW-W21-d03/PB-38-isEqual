634c2040ddd0d003c25ecb1d54316b791dde0ba1
OrhanKadirov <orhan.kadirov.gs@gmail.com>
2022-01-04 09:26:13 +0100

    Orhan's lösung

--- javascript-isEqual.js
+++ javascript-isEqual.js
@@ -1,6 +1,6 @@
 // Aufgaabe 1
-function isEqual(param1, param2) {
-    if ((param1 || param2) % 2 == 0) {
+function isEqual(number) {
+    if (number % 2 == 0) {
         console.log("Deine zahl ist gerade")
     } else {
         console.log("Deine zahl ist ungerade")
