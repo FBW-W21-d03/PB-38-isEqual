6b8b3d2fa493e8092a9a912660a293fc5a7c574e
kawanaza <kawanbabani@gmail.com>
2022-01-04 09:28:57 +0100

    Done

---
+++ index.js
@@ -0,0 +1,13 @@
+////Aufgabenstellung - Teil 1///
+
+function isEqual(number) {
+  if (number % 2 == 0) {
+    console.log("Die zahl ist gerade");
+  } else {
+    console.log("Die zahl ist ungerade");
+  }
+}
+
+/// Aufgabenstellung - Teil 2//
+isEqual(14);
+isEqual(17);
