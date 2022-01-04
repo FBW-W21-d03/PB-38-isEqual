9ca2cfaafb779ee5b4f0280be3bf04d31102d099
sprotte <metatron1972@live.de>
2022-01-04 09:27:01 +0100

    sprotte

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
+/*
+
 function isEqual() {
     let resultat = gradeZahl * ungradeZahl ;
     console.log("isEqual",resultat);
   }
-  
-  
-  
+    
   ergebniss();
+
+*/
+
+
+function istgerade(wert) {
+    if (wert%2 == 0)
+     console.log("gerade");
+      else{
+     console.log("ungerade") ;
+   }
+   }
+   istgerade(14);
+   istgerade(17);
