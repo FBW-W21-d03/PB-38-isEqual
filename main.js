ca3b5d4bbdf3385a976fca4655ef33047893f5ba
Dennisbullys <d.rochus@hotmail.de>
2022-01-04 09:27:35 +0100

    Dennis

---
+++ main.js
@@ -0,0 +1,13 @@
+
+//  Aufgabe 1
+
+function isEqual(value) {
+    if (value %2 == 0) 
+    console.log("Gerade Zahl")
+    else console.log("Ungerade Zahl")
+}
+isEqual(14)
+
+// Aufgabe 2
+
+isEqual(17)
