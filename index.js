59f29cc81123ef9caa80e7fde78a12e8593c702d
aisha2000-hub <aisha.noori2000@gmail.com>
2022-01-04 09:25:46 +0100

    Die lösung von Aisha

---
+++ index.js
@@ -0,0 +1,12 @@
+// AUFGABE TEIL-1
+function isEqual(number){
+    if(number % 2 == 0){
+        console.log("Die Zahl ist gerade")
+    }else{
+        console.log("Die Zahl ist ungerade")
+    }
+}
+
+// AUFGABE TEIL -2
+isEqual(14);
+isEqual(17);
