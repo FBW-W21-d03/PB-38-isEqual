e8ae7ab44239ea49c431d262a4aa55e1a9fbae90
Mak513 <89070265+Mak513@users.noreply.github.com>
2022-01-04 09:34:50 +0100

    Create osei.js

---
+++ osei.js
@@ -0,0 +1,11 @@
+function isEqual(number) {
+    if (number %2 === 0) {
+        console.log(number, "ist eine gerade Zahl");
+    }
+    else{
+        console.log(number, "ist eine ungeade Zahl");
+    }
+}
+
+isEqual(14);
+isEqual(17);
