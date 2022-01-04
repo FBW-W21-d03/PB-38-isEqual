33616d63550e39547e3a39fdcf03ffcdaacf0c92
Christian Heinrich <christian.heinrich@digitalcareerinstitute.org>
2022-01-04 09:24:21 +0100

    Lösung von Christian§

---
+++ christian.js
@@ -0,0 +1,10 @@
+function isEqual(number) {
+  if (number % 2 == 0) {
+    console.log("Die Zahl " + number + " ist gerade.");
+  } else {
+    console.log("Die Zahl " + number + " ist ungerade.");
+  }
+}
+
+isEqual(14);
+isEqual(17);
