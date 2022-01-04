0ead9b5eaaa15181104b12a80265809762887d3d
slavablack <veaceslav.ciornii@magenta.de>
2022-01-04 09:23:09 +0100

    Veaceslav

---
+++ index.js
@@ -0,0 +1,9 @@
+function isEqual (number) {
+    if (number %2) {
+        console.log("Die zahl ist gerade"); 
+    } else {
+        console.log("Die zahl ist ungerade");
+    }
+};
+isEqual (14);
+isEqual (17);
