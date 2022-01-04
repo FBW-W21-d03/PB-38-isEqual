68811b96ba3b51e35a4032226f612d2885191733
OrhanKadirov <orhan.kadirov.gs@gmail.com>
2022-01-04 09:23:53 +0100

    Orhan's lösung

---
+++ javascript-isEqual.js
@@ -0,0 +1,13 @@
+// Aufgaabe 1
+function isEqual(param1, param2) {
+    if ((param1 || param2) % 2 == 0) {
+        console.log("Deine zahl ist gerade")
+    } else {
+        console.log("Deine zahl ist ungerade")
+    }
+}
+
+// Aufgaabe 2
+
+isEqual(14);
+isEqual(17);
