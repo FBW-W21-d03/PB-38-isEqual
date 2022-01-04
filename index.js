35f002e2b84903a4d323c6b6c3e4664847372aa1
Mohal1125 <rajwindersinghmohal@yahoo.com>
2022-01-04 09:28:47 +0100

    fertig

---
+++ index.js
@@ -0,0 +1,9 @@
+function isEqual(number) {
+  if (number%2 == 0) {
+      console.log("Die Zahl ist gerade")
+  }else {
+      console.log("Die Zahl ist ungerade")
+  } 
+}
+isEqual(14);
+isEqual(17);
